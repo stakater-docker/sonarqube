db2b3f9ab040cd74ebc94583a3a201c58bd4ab9b
Hazim Malik <hazim_malik@hotmail.com>
2018-11-09 15:20:31 +0100

    Update script

--- run.sh
+++ run.sh
@@ -6,9 +6,7 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-chown -R sonarqube:sonarqube $SONARQUBE_HOME
-exec gosu sonarqube \
-  java -jar lib/sonar-application-$SONAR_VERSION.jar \
+exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
