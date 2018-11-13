9bc8e70432f6fc8b4b3ce9fda14d410b84dc828b
Hazim Malik <hazim_malik@hotmail.com>
2018-11-13 13:44:50 +0100

    WIP: move properties file if exists

--- run.sh
+++ run.sh
@@ -10,6 +10,13 @@ fi
 # Install plugins from download dir
 mv ${HOME}/downloads/plugins/* ${SONARQUBE_HOME}/extensions/plugins
 
+# WIP
+if [ -f /opt/app/tmp/conf/sonar.properties ];
+then
+  echo "moving properties"
+  mv /opt/app/tmp/conf/sonar.properties ${SONARQUBE_HOME}/conf/
+fi
+
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
